bbb82cc854be9f9d9a66902940f7c01814af8794
IdoCohen7 <idocohen727@gmail.com>
2024-06-14 16:45:47 +0300

    Commit to main - Ido

--- IdoAyaUdemy/IdoAyaUdemy/UdemyApp/UdemyApp/obj/Debug/net6.0/UdemyApp.AssemblyInfo.cs
+++ IdoAyaUdemy/IdoAyaUdemy/UdemyApp/UdemyApp/obj/Debug/net6.0/UdemyApp.AssemblyInfo.cs
@@ -15,7 +15,11 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dce2d1ac15eabdafa69ccde213d55e2baf14e28f")]
+=======
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+18188bc35521e6bc92305848e788ae6073007f75")]
+>>>>>>> Stashed changes
 =======
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+18188bc35521e6bc92305848e788ae6073007f75")]
 >>>>>>> Stashed changes
