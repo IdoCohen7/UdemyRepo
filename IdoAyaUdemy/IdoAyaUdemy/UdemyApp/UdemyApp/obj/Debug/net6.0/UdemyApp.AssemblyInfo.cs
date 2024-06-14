3d7bd6118c49204eb9f5fd7681def5b621a3ac7a
IdoCohen7 <idocohen727@gmail.com>
2024-06-14 16:27:04 +0300

    commit from branch
    ***

--- IdoAyaUdemy/IdoAyaUdemy/UdemyApp/UdemyApp/obj/Debug/net6.0/UdemyApp.AssemblyInfo.cs
+++ IdoAyaUdemy/IdoAyaUdemy/UdemyApp/UdemyApp/obj/Debug/net6.0/UdemyApp.AssemblyInfo.cs
@@ -14,7 +14,11 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UdemyApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
+<<<<<<< Updated upstream
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dce2d1ac15eabdafa69ccde213d55e2baf14e28f")]
+=======
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+18188bc35521e6bc92305848e788ae6073007f75")]
+>>>>>>> Stashed changes
 [assembly: System.Reflection.AssemblyProductAttribute("UdemyApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UdemyApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
